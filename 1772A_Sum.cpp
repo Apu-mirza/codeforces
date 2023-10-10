bb94a5c23fdee3568d24520c45b1e01cceb4c832
Apu-mirza <99646759+Apu-mirza@users.noreply.github.com>
2023-10-10 18:40:08 +0600

    1772A_Sum.cpp

---
+++ 1772A_Sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t, n, i;
+    cin>>t;
+    while(t--){
+        int a, b, c;
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+}
