3b7185090e0eb2c2d653371be151ff3de9292cc9
Apu-mirza <99646759+Apu-mirza@users.noreply.github.com>
2023-10-17 00:15:20 +0600

    Update 1729A_TwoElevators.cpp

--- 1729A_TwoElevators.cpp
+++ 1729A_TwoElevators.cpp
@@ -9,23 +9,19 @@ int main()
         int a, b, c;
         int time1, time2;
         cin>>a>>b>>c;
-        if(a == 1){
-            cout<<"1"<<endl;
-        }else{
             time1 = a-1;
             if(b > c){
                 time2 = b-1;
             }
             else{
-                time2 = 2*c-1;
+                time2 = 2*c-b-1;
             }
-        }
 
         if(time1 > time2){
             cout<<"2"<<endl;
         }else if(time1 == time2){
             cout<<"3"<<endl;
         }
-        else cout<<"1"<<endl;
+        else if(time1 < time2) {cout<<"1"<<endl;}
     }
 }
