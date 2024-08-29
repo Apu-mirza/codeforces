dad700fe6a40d509cfc7dcc351dca07799def2f3
Mirza Opu <99646759+Apu-mirza@users.noreply.github.com>
2024-08-30 00:32:45 +0600

    Update 349A Cinema LIne.cpp

--- 349A Cinema LIne.cpp
+++ 349A Cinema LIne.cpp
@@ -9,15 +9,31 @@ int main()
         cin>>a[i];
     }
     long long int checker = 25, flag = 0;
+    int t25=0, t50=0, t100=0;
     for(i=0; i<n; i++){
-        if(checker < a[i]){
+        if(a[i] == 25 && a[i] <= checker){
+            t25++;
+            checker += 25;
+            flag = 1;
+        }else if(a[i] == 50 && a[i] <= checker && t25 >= 1){
+            t25--;
+            checker += 25;
+            t50++;
+            flag = 1;
+        }else if(a[i] == 100 && a[i] <= checker && (t25>=1 && t50 >= 1)){
+            checker += 25;
+            t25--;
+            t50--;
+            flag = 1;
+        }else if(a[i] == 100 && a[i] <= checker && t25>=3){
+            t25 -= 3;
+            checker += 25;
+            flag = 1;
+        }else{
             flag = 0;
             break;
-        }else{
-            flag = 1;
-            checker += 25;
         }
     }
-    if(flag) cout<<"YES"<<endl;
+    if(flag  == 1) cout<<"YES"<<endl;
     else cout<<"NO"<<endl;
 }
